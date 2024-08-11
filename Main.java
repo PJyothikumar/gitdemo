d240abf0cc03c2a268ba9b73a2e452021fb57810
JK <jyokumar133@gmail.com>
2024-08-11 21:50:33 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+public static void main(string[] args){
+System.out.pri("Hello Git");}
+}[D[D[C[D[D[D[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
+}
